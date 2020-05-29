3b16c403a39484493078b4cc32d4bcd97e20b3cd
kapilraina <37952733+kapilraina@users.noreply.github.com>
2020-05-29 18:08:17 +0530

    APIURL

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://CHANGEME.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://161eb65b.eu-gb.apigw.appdomain.cloud/guestbook/entries';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
